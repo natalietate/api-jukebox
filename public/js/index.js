0bb74bb53cf77e5d1fe5cbc3b9b26eb7ab74d055
natalietate <martin.nat@gmail.com>
2017-10-19 15:12:06 -0400

    multiple songs

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 // all of your code needs to be inside this ENV function
 // variables for dubugging purposes
-var track;
-var trackList;
-var soundCloud;
+// var track;
+// var trackList;
+// var soundCloud;
 
 
 (function(ENV) {
@@ -11,36 +11,50 @@ var soundCloud;
     client_id: client_id
   });
 
-  // stream track id 293
-  SC.stream('/tracks/293').then(function(player) {
-    var play = document.querySelector('#play');
-    var pause = document.querySelector('#pause');
-    var stop = document.querySelector('#stop');
-
-    function playSong() {
-      player.play()
-    }
-
-    function pauseSong() {
-      player.pause()
-    }
-
-    play.addEventListener('click', playSong);
-    pause.addEventListener('click', pauseSong);
-
-  });
-
   // Remember, tracks are returned as an array
   SC.get('/tracks', {
     q: 'smooth jazz',
   }).then(function(tracks) {
-    let firstTrack = tracks[1];
+    let firstTrack = tracks[0];
+    let secondTrack = tracks[1];
 
     SC.stream('/tracks/' + firstTrack.id).then(function(player) {
+      let play = document.querySelector('#play');
+      let pause = document.querySelector('#pause');
+      let stop = document.querySelector('#stop');
 
+      function playSong() {
+        player.play()
+        document.querySelector('.currently-playing').innerHTML = "Now playing: " + firstTrack.title + " by " + firstTrack.artist
+      }
+
+      function pauseSong() {
+        player.pause()
+      }
+
+      play.addEventListener('click', playSong);
+      pause.addEventListener('click', pauseSong);
+    });
+
+    SC.stream('/tracks/' + secondTrack.id).then(function(player) {
+      let one = document.querySelector('#one');
+      let pause = document.querySelector('#pause');
+
+      function playSong() {
+        player.play()
+        document.querySelector('.currently-playing').innerHTML = "Now playing: " + secondTrack.title + " by " + secondTrack.artist
+      }
+
+      function pauseSong() {
+        player.pause()
+      }
+
+      one.addEventListener('click', playSong);
+      pause.addEventListener('click', pauseSong);
 
     });
   });
+
 
 
 })(ENV)
