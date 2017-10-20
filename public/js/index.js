c40208cd8e0f2a58fd4dd1ed1d37ca32c45aac7e
natalietate <martin.nat@gmail.com>
2017-10-20 16:14:14 -0400

    update track info display

--- public/js/index.js
+++ public/js/index.js
@@ -18,8 +18,9 @@ SC.get('/tracks', {
 
     function playSong() {
       player.play()
-      currentlyPlaying.innerHTML = "Now playing: " + tracks[i].title + " uploaded by " + tracks[i].user.username
+      currentlyPlaying.innerHTML = "Now playing: <a href=" + tracks[i].permalink_url + ">" + tracks[i].title + " </a> uploaded by <a href=" +tracks[i].user.permalink_url + ">" + tracks[i].user.username + "</a>"
       artwork.innerHTML = "<img src=" + tracks[i].artwork_url + ">";
+      info.innerHTML = "Genre: " + tracks[i].genre
     }
 
     function pauseSong() {
@@ -30,8 +31,9 @@ SC.get('/tracks', {
       i++
       SC.stream('/tracks/' + tracks[i].id).then(function(player) {
         console.log(tracks[i].id)
-        currentlyPlaying.innerHTML = "Now playing: " + tracks[i].title + " uploaded by " + tracks[i].user.username
+        currentlyPlaying.innerHTML = "Now playing: <a href=" + tracks[i].permalink_url + ">" + tracks[i].title + " </a> uploaded by <a href=" +tracks[i].user.permalink_url + ">" + tracks[i].user.username + "</a>"
         artwork.innerHTML = "<img src=" + tracks[i].artwork_url + ">";
+        info.innerHTML = "Genre: " + tracks[i].genre
         player.play()
         pause.addEventListener('click', function() {
           player.pause()
@@ -42,11 +44,20 @@ SC.get('/tracks', {
     let play = document.querySelector('#play');
     let pause = document.querySelector('#pause');
     let next = document.querySelector('#next');
-    let currentlyPlaying = document.querySelector('.currently-playing')
+    let currentlyPlaying = document.querySelector('.currently-playing');
     let artwork = document.querySelector('.artwork');
+    let info = document.querySelector('.track-info');
     play.addEventListener('click', playSong);
     pause.addEventListener('click', pauseSong);
     next.addEventListener('click', nextSong)
+
+
+
+
+//     Artist name with link to his/her profile page
+// Title with link to track's page
+// Description, genre and release date
+// Artwork
 
 
   });
